1608670b97cd3079e207e2875abc074f9d1be0f7
Alex Xiong <2428545784@qq.com>
2022-02-09 23:06:18 +1300

    complete model head

--- models.py
+++ models.py
@@ -9,4 +9,32 @@ class Heads(nn.Module):
 
         self.num_class = num_class
 
+        # branch 1 - classification and centerness
+        self.branch_conv_chunk =  nn.Sequential(
+            nn.Conv2d(256, 256, 1),
+            nn.Conv2d(256, 256, 1),
+            nn.Conv2d(256, 256, 1),
+            nn.Conv2d(256, 256, 1)
+        )
+
+        # branch 2 - regression
+        # same as the one above
+
+        # output part
+        self.reg = nn.Conv2d(256, 4, 1)
+        self.centerness = nn.Conv2d(256, 1, 1)
+        self.classification = nn.Conv2d(256, self.num_class, 1)
+    
+    def forward(self, x):
+        x = self.branch_conv_chunk(x)
         
+        reg = self.reg(x)
+        centerness = self.centerness(x)
+        classification = self.classification(x)
+
+        reg = nn.Sigmoid(reg)
+        centerness = nn.Sigmoid(centerness)
+        classification = torch.exp(classification)  # why use torch.exp?
+
+        return torch.cat([reg, centerness, classification], dim=1)
+
