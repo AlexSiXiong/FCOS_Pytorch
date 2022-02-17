373ba5fe5f486e981e826fb800f0b8fa4a03bdd3
Alex Xiong <2428545784@qq.com>
2022-02-17 23:48:16 +1300

    data loader init

---
+++ dataloader.py
@@ -0,0 +1,26 @@
+from logging import root
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+import os, io
+
+
+class FCOSDataset(Dataset):
+    def __init__(self, csv_file, root_dir, num_classes=10) -> None:
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+        """
+        self.object_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.num_classes = num_classes
+        
+    def __len__(self) -> int:
+        return len()
+
+    def _process_img(self):
+        pass
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, self.object_frame.iloc[idx, 0])
+        image = io.imread(img_name)
