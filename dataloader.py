b3515cdb030981bc66d2f7a5a3524d40eb79db26
Alex Xiong <2428545784@qq.com>
2022-02-23 08:51:47 +1300

    complete BananaDataset

--- dataloader.py
+++ dataloader.py
@@ -11,7 +11,35 @@ http://d2l-data.s3-accelerate.amazonaws.com/banana-detection.zip
 Ped Data resource:
 https://www.cis.upenn.edu/~jshi/ped_html/
 """
-class FCOSDataset(torch.utils.data.Dataset):
+
+def read_banana(is_train=True, root_dir='data/banana/'):
+    file_name = os.path.join(root_dir, 'bananas_train' if is_train else 'bananas_val', 'label.csv')
+    csv_data = pd.read_csv(file_name)
+    csv_data = csv_data.set_index('image_name')
+
+    images, label_position = [], []
+
+    for image_name, label, position in csv_data.iterrows:
+        img = Image.open(os.path.join(root_dir, 'bananas_train' if is_train else 'bananas_val', 'images', f'{image_name}')).convert('RGB')
+        images.append(img)
+
+        label_position.append([label, position])
+
+    return images, torch.tensor(label_position).unsqueeze(1)/256
+
+class BananasDataset(torch.utils.data.Dataset):
+    def __init__(self, is_train, root_dir='data/banana/') -> None:
+        self.images, self.label_position = read_banana(is_train, root_dir)
+        print('read ' + str(len(self.features)) + (f' training examples' if is_train else f' validation examples'))
+
+    def __getitem__(self, idx):
+        return (self.images[idx].float(), self.label_position[idx])
+
+    def __len__(self):
+        return len(self.images)
+
+
+class CampusPedestrianDataset(torch.utils.data.Dataset):
     def __init__(self, csv_file, root_dir, num_classes=10) -> None:
         """
         Args:
