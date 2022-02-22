dae14acd018fda855badae712d8c54e9e12e9688
Alex Xiong <2428545784@qq.com>
2022-02-23 00:41:24 +1300

    prepare the data set

--- dataloader.py
+++ dataloader.py
@@ -1,9 +1,16 @@
 from logging import root
 from torch.utils.data import DataLoader
 import pandas as pd
-import os, io
+import os
+from PIL import Image
 
+"""
+Banana Data resource:
+http://d2l-data.s3-accelerate.amazonaws.com/banana-detection.zip
 
+Ped Data resource:
+https://www.cis.upenn.edu/~jshi/ped_html/
+"""
 class FCOSDataset(torch.utils.data.Dataset):
     def __init__(self, csv_file, root_dir, num_classes=10) -> None:
         """
@@ -15,6 +22,7 @@ class FCOSDataset(torch.utils.data.Dataset):
         self.root_dir = root_dir
         self.num_classes = num_classes
         
+        
     def __len__(self) -> int:
         return len()
 
@@ -22,5 +30,9 @@ class FCOSDataset(torch.utils.data.Dataset):
         pass
 
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, self.object_frame.iloc[idx, 0])
-        image = io.imread(img_name)
+        image_path = os.path.join(self.root_dir, self.object_frame.iloc[idx, 0])
+        image = Image.open(image_path).convert('RGB')
+
+
+        num_objects=len(object_ids)
+
